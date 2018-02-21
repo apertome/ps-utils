daa8a322ca8e51e77e0d6cf9113e6c4b42e82792
Michael Johnson <mj82@grnoc.iu.edu>
2018-02-21 16:30:52 +0000

    Add runtests.pl for easily running multiple tests between a list of hosts

---
+++ bin/runtests.pl
@@ -0,0 +1,59 @@
+#!/usr/bin/perl
+
+# Given an array of hostnames/addresses, this script runs throughput tests
+# between all the hosts, in both directions
+
+my @hosts = (
+    'host1',
+    'host2'
+);
+
+# flags to send to pscheduler. here we're setting the window to 128M
+my $pscheduler_flags = '-w 128M';
+
+# currently we're hard-coded to throughput but should make this more flexible
+my $pscheduler = '/usr/bin/pscheduler task throughput ';
+
+my $source  = $hosts[0];
+my $destination = $hosts[1];
+
+foreach my $ordered ([$source, $destination], [$destination, $source]) {
+    my ($src, $dst) = @$ordered;
+
+    print "Source: $src\n";
+    print "Destination: $dst\n";
+
+    my $command = $pscheduler . " --source $src --dest $dst ";
+    $command .= $pscheduler_flags;
+
+    print "Command: " . $command . "\n";
+    my $output = `$command`;
+    print "Output:\n$output\n";
+}
+
+foreach my $source( @end_hosts ) {
+    foreach my $destination( @hosts ) {
+        foreach my $ordered ([$source, $destination], [$destination, $source]) {
+            my ($src, $dst) = @$ordered;
+
+            print "Source: $src\n";
+            print "Destination: $dst\n";
+
+            my $command = $pscheduler . " --source $src --dest $dst ";
+            $command .= $pscheduler_flags;
+
+            print "Command: " . $command . "\n";
+            my $output = `$command`;
+            print "Output:\n$output\n";
+        }
+
+
+    }
+
+}
+
+
+sub host_pairs {
+    my ($hosts) = @_;
+
+}
